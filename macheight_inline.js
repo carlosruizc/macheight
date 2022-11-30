30a20f86b3d0e970203eb61b21eaa4ae8a122c00
Carlos Ruiz <caroru1974@gail.com>
2022-11-30 16:32:24 -0500

    restore previous version

--- macheight_inline.js
+++ macheight_inline.js
@@ -1,21 +1,14 @@
-const getPairs = () => {
-    const args = process.argv.slice(2);
-    let numbers = args[0].split(',').map(function (string) {
-        return parseInt(string);
-    });
-    let targetSum = parseInt(args[1]);
-    let totalPairs = [];
-    let pair = [];
-    for (let i = 0; i < numbers.length; i++) {
-        for (let j = i + 1; j < numbers.length; j++) {
-            if (numbers[i] + numbers[j] === targetSum) {
-                console.log(numbers[i] + "," + numbers[j]);
-                pair.push(numbers[i]);
-                pair.push(numbers[j]);
-                totalPairs.push[pair];
-                pair = [];
-            }
+const args = process.argv.slice(2);
+let numbers = args[0].split(',').map(function (string) {
+    return parseInt(string);
+});
+let targetSum = parseInt(args[1]);
+let totalPairs = [];
+let pair = [];
+for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+        if (numbers[i] + numbers[j] === targetSum) {
+            console.log(numbers[i] + "," + numbers[j]);
         }
     }
-    return totalPairs;
-};
+}
