efc2803397c89dfc47997b48293e3fffba4f7b6e
carlosruizc <48249748+carlosruizc@users.noreply.github.com>
2022-11-30 15:22:49 -0500

    Add files via upload

---
+++ macheight.js
@@ -0,0 +1,18 @@
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+let inputNumbers = prompt('Input the list of numbers: ');
+let inputTarget = prompt('Input the target value: ');
+numbers = inputNumbers.split(',').map(function (string) {
+    return parseInt(string);
+});
+let targetSum = parseInt(inputTarget);
+let totalPairs = [];
+let pair = [];
+for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+        if (numbers[i] + numbers[j] === targetSum) {
+            console.log(numbers[i] + "," + numbers[j]);
+        }
+    }
+}
